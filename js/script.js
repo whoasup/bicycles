5bf8bcf7783fcf17d1cb718e07ef7fe6f001bdba
Semen Panfilov <whoasup@gmail.com>
2021-01-27 08:39:55 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+(function () {
+  var menu = document.querySelector('.menu');
+  var burger = document.querySelector('.burger');
+  var menuItems = menu.querySelectorAll('.menu__link');
+  var pageBody = document.querySelector('body');
+
+  var changeBurger = function () {
+    var expanded = burger.getAttribute('aria-expanded') === 'true';
+    burger.setAttribute('aria-expanded', !expanded);
+  };
+
+  var toggleMenu = function () {
+    changeBurger();
+    burger.classList.toggle('burger--open');
+    menu.classList.toggle('menu--close');
+    pageBody.classList.toggle('no-scroll');
+  };
+
+  var onMenuItemClick = function () {
+    if (!menu.classList.contains('menu--close')) {
+      toggleMenu();
+    }
+  };
+
+  menu.classList.remove('menu--no-js');
+  menu.classList.toggle('menu--close');
+
+  burger.addEventListener('click', function () {
+    toggleMenu();
+  });
+
+  for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', onMenuItemClick);
+  }
+})();
